f060508a00e149d08b5c8fd59d9725cfffa2c6cc
Vishal Kumar <21ucc116@lnmiit.ac.in>
2022-11-08 16:12:33 +0530

    Changes

--- BTPproject/src/tpo_pages/tpo_page.js
+++ BTPproject/src/tpo_pages/tpo_page.js
@@ -1,14 +1,16 @@
 import React from "react";
 import tpoCSS from "./tpo_page.module.css"
+import Sidebar from "./components/Sidebar"
+import { Outlet } from "react-router-dom";
 
 export default function tpo() {
       return (
           <div className={tpoCSS.main}>
             <div className={tpoCSS.sidebar}>
-                Hello
+                <Sidebar />
             </div>
             <div className={tpoCSS.pages}>
-                Hello
+                <Outlet/>
             </div>
           </div>  
       );
