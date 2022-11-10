040b19ca8a264e3e4a37a01a7f3747b6d1b6577e
Ramsha42001 <ramshaiqbal4@gmail.com>
2022-11-10 19:05:53 +0530

    some minor changes in styling of top pages div

--- BTPproject/src/tpo_pages/components/Student_varification_list.js
+++ BTPproject/src/tpo_pages/components/Student_varification_list.js
@@ -6,16 +6,17 @@ import Navbar from "./Navbar1";
 export default function Student_varification_list () {
     return (
 <>
-<div className={Student_varification_list_css.list_heading}>
+<div div className={Student_varification_list_css.list_heading}>
+
     <p  className={Student_varification_list_css.list_1}>#</p>
     <p  className={Student_varification_list_css.list_2}>Roll no</p>
     <p  className={Student_varification_list_css.list_3}>Name</p>
     <p  className={Student_varification_list_css.list_4}>Phone No</p>
     <p  className={Student_varification_list_css.list_5}>Gender</p>
     <p  className={Student_varification_list_css.list_6}>Branch</p>
-</div>
-<div >
+    </div>
 
+<div>
 {
         Data.map((value)=>(
         <div key= {value.id} className={Student_varification_list_css.child}>
